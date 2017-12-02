5cf26c192cc3b9273a100ef33aff6720ec418dcc
Justin Li <justinnhli@gmail.com>
2017-12-02 14:58:27 -0800

    create module

--- testsuite.py
+++ testsuite.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-import search
+import algorithms
 import domains
 
 
@@ -13,17 +13,17 @@ class Search_GridWorld(unittest.TestCase):
         self.goal_state = domains.GridWorldState(19, 15, 20, 20)
 
     def test_bfs(self):
-        final_node = search.GoalOrientedBreadthFirstSearch(goal_state=self.goal_state).search_node(self.start_state)
+        final_node = algorithms.GoalOrientedBreadthFirstSearch(goal_state=self.goal_state).search_node(self.start_state)
         self.assertIsNotNone(final_node)
         self.assertEqual(len(final_node.path()), 35)
 
     def test_ucs(self):
-        final_node = search.GoalOrientedUniformCostSearch(goal_state=self.goal_state).search_node(self.start_state)
+        final_node = algorithms.GoalOrientedUniformCostSearch(goal_state=self.goal_state).search_node(self.start_state)
         self.assertIsNotNone(final_node)
         self.assertEqual(len(final_node.path()), 35)
 
     def test_astar(self):
-        final_node = search.AStarSearch(goal_state=self.goal_state).search_node(self.start_state)
+        final_node = algorithms.AStarSearch(goal_state=self.goal_state).search_node(self.start_state)
         self.assertIsNotNone(final_node)
         self.assertEqual(len(final_node.path()), 35)
 
@@ -35,7 +35,7 @@ class Search_SlidingPuzzle(unittest.TestCase):
         self.goal_state = domains.SlidingPuzzleState(' 12345678')
 
     def test_astar(self):
-        final_node = search.AStarSearch(goal_state=self.goal_state).search_node(self.start_state)
+        final_node = algorithms.AStarSearch(goal_state=self.goal_state).search_node(self.start_state)
         self.assertIsNotNone(final_node)
         self.assertEqual(len(final_node.path()), 21)
 
@@ -45,17 +45,17 @@ class Search_Polynomial(unittest.TestCase):
     def test_beam_left(self):
         start_state = domains.PolynomialDescentState(-10)
         goal_state = domains.PolynomialDescentState(-1)
-        self.assertEqual(search.HillClimbing().search(start_state), goal_state)
+        self.assertEqual(algorithms.HillClimbing().search(start_state), goal_state)
 
     def test_beam_middle(self):
         start_state = domains.PolynomialDescentState(0)
         goal_state = domains.PolynomialDescentState(-1)
-        self.assertEqual(search.HillClimbing().search(start_state), goal_state)
+        self.assertEqual(algorithms.HillClimbing().search(start_state), goal_state)
 
     def test_beam_right(self):
         start_state = domains.PolynomialDescentState(10)
         goal_state = domains.PolynomialDescentState(-1)
-        self.assertEqual(search.HillClimbing().search(start_state), goal_state)
+        self.assertEqual(algorithms.HillClimbing().search(start_state), goal_state)
 
 
 if __name__ == "__main__":
